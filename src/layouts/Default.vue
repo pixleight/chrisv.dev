f33c6e81a8911ec21e77a2cf365a69f49bd04522
Chris Violette <chris.violette@the-refinery.io>
2020-01-10 14:03:33 -0500

    doing some header layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <slot/>
+  <div class="wrapper min-h-screen text-white bg-gray-900">
+    <header class="bg-purple-900">
+      <div class="fixed flex inset-0 p-4 w-48 border-t-4 border-b-4 border-purple-600">
+        <section class="relative z-10">
+          <h1>Header</h1>
+        </section>
+      </div>
+    </header>
+    <main class="p-4">
+      <slot/>
+    </main>
   </div>
 </template>
 
@@ -12,5 +21,34 @@ query {
 }
 </static-query>
 
-<style>
+<style scoped>
+  .wrapper {
+    display: grid;
+
+    @screen sm {
+      grid-template-columns: 12rem auto;
+      grid-column-gap: 6vh;
+    }
+  }
+
+  header div::after {
+    @apply absolute block inset-0 w-full bg-purple-900 border-t-4 border-b-4 border-purple-600;
+
+    top: -0.25rem;
+    bottom: -0.25rem;
+    content: '';
+    transform: skewX(-3deg);
+    transform-origin: bottom right;
+  }
+
+  header section {
+    @apply w-full;
+
+    transform: skewX(-3deg);
+    transform-origin: bottom right;
+  }
+
+  header section > * {
+    transform: skewX(3deg);
+  }
 </style>

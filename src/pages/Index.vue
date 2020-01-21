e4277e1e605579b32327dd125c0b9f1064486971
Chris Violette <chris.violette@the-refinery.io>
2020-01-20 21:50:45 -0500

    Lots more styling

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,10 +4,16 @@
       Index.vue
     </h1>
     <section>
-      <article class="relative bg-gray-500 rounded-lg shadow overflow-hidden" v-for="edge in $page.posts.edges" :key="edge.node.id">
-        {{ edge.node.title }}
-        <g-image :src="edge.node.avatar" class="w-full absolute inset-0 object-cover" />
-      </article>
+      <g-link class="block flex" v-for="edge in $page.posts.edges" :key="edge.node.id" :to="edge.node.id">
+        <article class="relative w-full bg-gray-500 rounded-lg shadow overflow-hidden flex flex-col hover:shadow-xl">
+          <h3 class="relative z-10 text-white mt-auto mb-4 mr-4 px-4 pt-2 pb-3">
+            <g-link :to="edge.node.slug">
+              <span>{{ edge.node.title }}</span>
+            </g-link>
+          </h3>
+          <g-image :src="edge.node.avatar" class="w-full absolute inset-0 object-cover" />
+        </article>
+      </g-link>
     </section>
   </Layout>
 </template>
@@ -41,23 +47,68 @@ section {
   position: relative;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
-  grid-auto-rows: 12rem;
+  grid-auto-rows: 15rem;
   grid-auto-flow: dense;
-  grid-gap: 1rem;
+  grid-gap: 3rem;
 }
 
 @screen sm {
   article:nth-child(7n) {
-    grid-column: span 2;
-    grid-row: span 2;
+    /* grid-column: span 2;
+    grid-row: span 2; */
   }
 }
 
 img {
-  transition: transform 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  transition: transform;
+  transition-duration: 200ms;
+  transition-timing-function: cubic-bezier(0.39, 0.575, 0.565, 1);
 }
 
-article:hover img {
-  transform: scale(1.3) rotate(7deg);
+
+article {
+  transition: all 200ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+}
+
+article:hover {
+  & img {
+    transform: scale(1.3) rotate(7deg);
+    transition-duration: 500ms;
+    transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+
+  & h3 {
+    &::before {
+      clip-path: polygon(0 0.5rem, 100% 0, calc(100% - 0.5rem) calc(100% - 0.5rem), 0 100%);
+    }
+  }
+}
+
+h3 {
+  &::before,
+  &::after {
+    @apply block absolute z-0 inset-0;
+    content: '';
+  }
+
+  &::before {
+    @apply bg-purple-900;
+    z-index: -1;
+    transition: clip-path 250ms;
+    transition-timing-function: cubic-bezier(0.25, 0.46, 0.45, 0.94);
+    clip-path: polygon(0 0.5rem, 0 0.5rem, 0 0.5rem, 0 100%);
+  }
+
+  &::after {
+    @apply bg-black;
+    opacity: 0.8;
+    z-index: -2;
+    clip-path: polygon(0 0.5rem, 100% 0, calc(100% - 0.5rem) calc(100% - 0.5rem), 0 100%);
+  }
+
+  & span {
+    display: block;
+    transform: rotate(-2deg);
+  }
 }
 </style>

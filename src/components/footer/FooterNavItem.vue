74bef4fd814d9348885f9aa8d5da7dda891b01fd
Chris Violette <chris.violette@the-refinery.io>
2020-03-10 23:40:17 -0400

    creating nav item component

---
+++ src/components/footer/FooterNavItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <g-link :to="to" class="inline-block px-2 py-1 uppercase relative hover:text-gray-900 transition-colors duration-200 ease-out">
+      <span class="relative z-10">
+        <slot />
+      </span>
+    </g-link>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      to: {
+        type: String,
+        required: true,
+      },
+    },
+  }
+</script>
+
+<style scoped>
+a::before {
+  @apply block absolute top-0 bottom-0 w-0 bg-yellow-500 duration-200 ease-out;
+  content: '';
+  left: 50%;
+  transform: translateX(-50%) skewX(25deg);
+  transition-property: width;
+}
+
+a:hover::before {
+  @apply w-full;
+}
+</style>
